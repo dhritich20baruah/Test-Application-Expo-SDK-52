287ea85b21ffc21942979049b7e7d76e29b7e742
Dhriti <dhriti.baruah20@gmail.com>
2024-12-30 08:54:09 +0530

    media permission

--- components/MediaLibraryCamera/CameraFunction.js
+++ components/MediaLibraryCamera/CameraFunction.js
@@ -1,10 +1,10 @@
+import { useState, useRef, useEffect } from "react";
 import {
   CameraView,
   CameraType,
   useCameraPermissions,
   Camera,
 } from "expo-camera";
-import { useState, useRef, useEffect } from "react";
 import {
   Button,
   StyleSheet,
@@ -17,37 +17,39 @@ import {
 } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import * as MediaLibrary from "expo-media-library";
-import { SQLiteProvider, useSQLiteContext } from "expo-sqlite";
+// import { SQLiteProvider, useSQLiteContext } from "expo-sqlite";
 
-const initializeDB = async (db) => {
-  try {
-    await db.execAsync(`
-        PRAGMA journal_mode = WAL;
-        CREATE TABLE IF NOT EXISTS gallery (id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, uri TEXT, notes TEXT);   
-        `);
-    console.log("DB connected");
-  } catch (error) {
-    console.log("Error in connecting DB", error);
-  }
-};
+// const initializeDB = async (db) => {
+//   try {
+//     await db.execAsync(`
+//         PRAGMA journal_mode = WAL;
+//         CREATE TABLE IF NOT EXISTS gallery (id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, uri TEXT, notes TEXT);   
+//         `);
+//     console.log("DB connected");
+//   } catch (error) {
+//     console.log("Error in connecting DB", error);
+//   }
+// };
 
-export default function CameraApp() {
-  return (
-    <SQLiteProvider databaseName="SDK52Test.db" onInit={initializeDB}>
-      <CameraFunction />
-    </SQLiteProvider>
-  );
-}
+// export default function CameraApp() {
+//   return (
+//     <SQLiteProvider databaseName="SDK52Test.db" onInit={initializeDB}>
+//       <CameraFunction />
+//     </SQLiteProvider>
+//   );
+// }
 
-export function CameraFunction() {
-  const db = useSQLiteContext();
+export default function CameraFunction() {
+  // const db = useSQLiteContext();
   const [facing, setFacing] = useState("back");
+  const [cameraMode, setCameraMode] = useState("picture");
   const [permission, requestPermission] = useCameraPermissions(); //Camera Permission State
   const [hasMediaLibPermit, setHasMediaLibPermit] = useState(); //Media Permission State
+  const [permissionResponse, requestPermissionRe] = MediaLibrary.usePermissions();
   let cameraRef = useRef();
   const [photo, setPhoto] = useState();
   const [flashMode, setFlashMode] = useState("on");
-  const [notes, setNotes] = useState("")
+  const [recording, setRecording] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -55,7 +57,7 @@ export function CameraFunction() {
       const mediaLibraryPermission =
         await MediaLibrary.requestPermissionsAsync();
       setHasCameraPermission(cameraPermission.status === "granted");
-      setHasMediaLibPermit(mediaLibraryPermission.status === "granted");
+      setHasMediaLibPermit(permissionResponse.status === "granted");
     })();
   }, []);
 
@@ -93,8 +95,29 @@ export function CameraFunction() {
 
     let newPhoto = await cameraRef.current.takePictureAsync(options);
     setPhoto(newPhoto);
-    console.log("URI", photo.uri)
+    console.log("URI", photo.uri);
   };
+
+  async function recordVideo() {
+    if (cameraRef) {
+      try {
+        setRecording(true);
+        const video = await cameraRef.current.recordAsync({
+          maxDuration: 10
+        })
+        console.log(video.uri); // Video file location
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+
+  function stopRecording() {
+    if (cameraRef) {
+      setRecording(false);
+      cameraRef.stopRecording();
+    }
+  }
 
   if (photo) {
     let savePhoto = async () => {
@@ -108,18 +131,7 @@ export function CameraFunction() {
     };
     return (
       <SafeAreaView style={styles.imageContainer}>
-        <Image
-          style={styles.preview}
-          source={{ uri: photo.uri }}
-        />
-        <View>
-          <TextInput
-            style={styles.input}
-            placeholder="Notes"
-            value={notes}
-            onChangeText={setNotes}
-          />
-        </View>
+        <Image style={styles.preview} source={{ uri: photo.uri }} />
         <View style={styles.btnContainer}>
           {!hasMediaLibPermit ? (
             <TouchableOpacity onPress={savePhoto} style={styles.btn}>
@@ -144,14 +156,23 @@ export function CameraFunction() {
         facing={facing}
         ref={cameraRef}
         flash={flashMode}
+        mode={cameraMode}
       >
-        <View>
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
             <Ionicons name="camera-reverse-outline" size={20} color="white" />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={takePic}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => setCameraMode("picture")}
+          >
             <Ionicons name="camera-outline" size={20} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => setCameraMode("video")}
+          >
+            <Ionicons name="videocam-outline" size={20} color="white" />
           </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={toggleFlash}>
             <Text>
@@ -174,10 +195,19 @@ export function CameraFunction() {
           </TouchableOpacity>
         </View>
         <View style={styles.shutterContainer}>
-        <TouchableOpacity style={styles.button} onPress={takePic}>
-            <Ionicons name="aperture-outline" size={40} color="white" />
-          </TouchableOpacity>
-        </View>
+          {cameraMode === "picture" ? (
+            <TouchableOpacity style={styles.button} onPress={takePic}>
+              <Ionicons name="aperture-outline" size={40} color="white" />
+            </TouchableOpacity>
+          ) : recording ? (
+            <TouchableOpacity style={styles.button} onPress={stopRecording}>
+              <Ionicons name="stop-circle-outline" size={40} color="white" />
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity style={styles.button} onPress={recordVideo}>
+              <Ionicons name="play-circle-outline" size={40} color="white" />
+            </TouchableOpacity>
+          )}
         </View>
       </CameraView>
     </View>
@@ -191,7 +221,7 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     height: "95%",
-    width: "100%"
+    width: "100%",
   },
   message: {
     textAlign: "center",
@@ -201,16 +231,17 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   buttonContainer: {
+    flex: 1,
     flexDirection: "row",
     backgroundColor: "transparent",
-    marginVertical: 20
+    margin: 20,
   },
   shutterContainer: {
     display: "flex",
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    margin: 20
+    margin: 20,
   },
   button: {
     flex: 1,
@@ -221,7 +252,7 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-evenly",
-    backgroundColor: "white"
+    backgroundColor: "white",
   },
   text: {
     fontSize: 24,
@@ -235,7 +266,7 @@ const styles = StyleSheet.create({
   },
   input: {
     borderColor: "black",
-    fontSize: 20
+    fontSize: 20,
   },
   preview: {
     alignSelf: "stretch",
