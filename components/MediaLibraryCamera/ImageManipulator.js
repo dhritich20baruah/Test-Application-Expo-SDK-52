463b3c09e4aaddf5446551db6e8c72cf33a62c2f
Dhriti <dhriti.baruah20@gmail.com>
2025-02-02 08:07:54 +0530

    Image manipulator set up

--- components/MediaLibraryCamera/ImageManipulator.js
+++ components/MediaLibraryCamera/ImageManipulator.js
@@ -1,8 +1,7 @@
-import { useState, useEffect } from 'react';
-import { Button, Image, StyleSheet, View } from 'react-native';
-import { Asset } from 'expo-asset';
-import { manipulateAsync, FlipType, SaveFormat } from 'expo-image-manipulator';
-import * as ImageManipulator from 'expo-image-manipulator';
+import { useState, useEffect } from "react";
+import { Button, Image, StyleSheet, View } from "react-native";
+import { Asset } from "expo-asset";
+import { manipulateAsync, FlipType, SaveFormat } from "expo-image-manipulator";
 
 export default function ImageManipulation() {
   const [ready, setReady] = useState(false);
@@ -10,30 +9,34 @@ export default function ImageManipulation() {
 
   useEffect(() => {
     (async () => {
-        const localImage = require('./assets/thumbnail.png');
-        setImage(localImage);
-        setReady(true);
+      const pic = Asset.fromModule(require("../../assets/thumbnail.png")).uri;
+      setImage({ uri: pic });
+      setReady(true);
     })();
   }, []);
 
   const _rotate90andFlip = async () => {
-    const manipResult = await ImageManipulator(
-      image.localUri || image.uri,
-      [{ rotate: 90 }, { flip: FlipType.Vertical }],
+    if (!image) return;
+    const manipResult = await manipulateAsync(
+      image.uri,
+      [
+        { rotate: 90 },
+        { flip: FlipType.Vertical },
+        // { crop: { originX: 50, originY: 50, width: 200, height: 200 } }, // Crop a section
+        // { resize: { width: 150, height: 150 } }, // Resize the image
+      ],
       { compress: 1, format: SaveFormat.PNG }
     );
     setImage(manipResult);
   };
 
-  const _renderImage = () => (
-    <View style={styles.imageContainer}>
-      <Image source={{ uri: image.localUri || image.uri }} style={styles.image} />
-    </View>
-  );
-
   return (
     <View style={styles.container}>
-      {ready && image && _renderImage()}
+      {ready && image && (
+        <View style={styles.imageContainer}>
+          <Image source={{ uri: image.uri }} style={styles.image} />
+        </View>
+      )}
       <Button title="Rotate and Flip" onPress={_rotate90andFlip} />
     </View>
   );
@@ -42,16 +45,16 @@ export default function ImageManipulation() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   imageContainer: {
     marginVertical: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   image: {
     width: 300,
     height: 300,
-    resizeMode: 'contain',
+    resizeMode: "contain",
   },
 });
