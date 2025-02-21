fc456d5844f8af8b85bba206c485e48c778364fe
Dhriti <dhriti.baruah20@gmail.com>
2025-02-21 22:05:48 +0530

    Scan result screen  design

--- components/QRCode/ScanResult.js
+++ components/QRCode/ScanResult.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { View, Text, Button, StyleSheet, Image } from "react-native";
+import { View, Text, Button, StyleSheet, Image, TouchableOpacity } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function ScanResult({route, navigation}){
     const { qrData } = route.params
     return(
-        <View>
-            <Text>{qrData}</Text>
+        <View style={{flex: 1, padding: 10, height: "100%"}}>
+            <Text style={{fontSize: "20", fontWeight: "600"}}>{qrData}</Text>
+            <View style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly", width: "100%"}}>
+                <TouchableOpacity style={{width:"30%"}}>
+                    <Text><Ionicons name="open" size={45} color="grey" /></Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{width:"30%"}}>
+                    <Text><Ionicons name="share-social" size={45} color="grey" /></Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{width:"30%"}}>
+                    <Text><Ionicons name="copy" size={45} color="grey" /></Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
