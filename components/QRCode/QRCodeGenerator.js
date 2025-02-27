d8d9afc8b38e370fed35baf4ab4230215fd0865c
Dhriti <dhriti.baruah20@gmail.com>
2025-02-27 21:58:40 +0530

    QR code download fixed

--- components/QRCode/QRCodeGenerator.js
+++ components/QRCode/QRCodeGenerator.js
@@ -1,13 +1,23 @@
-import React, { useState } from "react";
-import { Alert, Button, Text, TextInput, View } from "react-native";
+import React, { useState, useRef, useEffect } from "react";
+import { Alert, Button, Text, TextInput, View, Share } from "react-native";
 import QRCode from "react-native-qrcode-svg";
 import * as FileSystem from "expo-file-system";
-import * as Sharing from "expo-sharing";
+import ViewShot from "react-native-view-shot";
+import * as MediaLibrary from "expo-media-library";
 
 const QRCodeGenerator = () => {
   const [text, setText] = useState("");
   const [qRValue, setQRValue] = useState(null);
   const viewShotRef = useRef(null);
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState(); //State variable for media library permission
+
+  useEffect(() => {
+    (async () => {
+      const mediaLibraryPermission =
+        await MediaLibrary.requestPermissionsAsync();
+      setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+    })();
+  }, []);
 
   const generateQR = () => {
     if (!text.trim()) {
@@ -19,28 +29,26 @@ const QRCodeGenerator = () => {
 
   const downloadQRCode = async () => {
     try {
-      if (!viewShotRef.current || !qrValue) {
-        Alert.alert("Error", "Generate a QR Code first!");
+      if (!viewShotRef.current || !qRValue) {
+        Alert.alert("Error", "Generate a QR Code first");
         return;
       }
 
       const uri = await viewShotRef.current.capture();
       const filePath = `${FileSystem.cacheDirectory}QRCode.jpg`;
 
-      await FileSystem.moveAsync({ from: uri, to: filePath });
-
-      await Share.share({
-        url: filePath,
-        message: "Here is your QR Code!",
+      await MediaLibrary.saveToLibraryAsync(filePath).then(() => {
+        Alert.alert("QR Code saved to Media Library");
       });
     } catch {
       console.error("Error saving QR Code:", error);
+      Alert.alert("Error saving QR Code", error.message);
     }
   };
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ margin: 10 }}>QRCodeGenerator</Text>
+      <Text style={{ margin: 10 }}>Enter Link to Create QR Code for:</Text>
       <TextInput
         placeholder="Enter Text for QR Code"
         value={text}
@@ -57,10 +65,18 @@ const QRCodeGenerator = () => {
       <Button title="Generate QR Code" onPress={generateQR} color="black" />
       {qRValue && (
         <View style={{ margin: 20 }}>
-          <ViewShot ref={viewShotRef} options={{ format: "jpg", quality: 1.0 }}>
+          <ViewShot
+            ref={viewShotRef}
+            options={{ format: "jpg", quality: 1.0 }}
+            style={{ marginVertical: 20 }}
+          >
             <QRCode value={qRValue} size={200} />
           </ViewShot>
-          <Button title="Download QR Code" onPress={downloadQRCode} />
+          <Button
+            title="Download QR Code"
+            onPress={downloadQRCode}
+            color="black"
+          />
         </View>
       )}
     </View>
